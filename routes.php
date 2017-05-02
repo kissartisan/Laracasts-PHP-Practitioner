85f1faf71309117a4f46f9a2d86b3ea8f37fe17e
Reymark Torres <rtorres@i4asiacorp.com>
2017-05-02 19:20:46 +0800

    Add lesson 1-17

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    '~raymart/Laracasts/Laravel/PHP_practitioner' => 'controllers/index.php',
+    '~raymart/Laracasts/Laravel/PHP_practitioner/about' => 'controllers/about.php',
+    '~raymart/Laracasts/Laravel/PHP_practitioner/about/culture' => 'controllers/about-culture.php',
+    '~raymart/Laracasts/Laravel/PHP_practitioner/contact' => 'controllers/contact.php'
+]);
